37f55f96905257528ba8e3e4b94fb0446d09bca7
Irakliy Kukava <irakliy.kukava@gmail.com>
2019-02-17 20:23:05 +0300

    Correct a mistake in exception class description

--- Server/src/com/irakliy01/messenger/server/UserExistsException.java
+++ Server/src/com/irakliy01/messenger/server/UserExistsException.java
@@ -3,7 +3,7 @@ package com.irakliy01.messenger.server;
 import java.net.InetAddress;
 
 /**
- * Custom exception that must be thrown when new user tries to connect to the server and user with such IP address if already connected to the server
+ * Custom exception that must be thrown when new user tries to connect to the server and user with such IP address is already connected to the server
  */
 public class UserExistsException extends Exception {
 
